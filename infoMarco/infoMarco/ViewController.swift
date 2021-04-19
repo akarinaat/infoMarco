fa19e4a384e5221c3c6b5e6bdd8988d92f0d5eb5
Karina Amador <akarina.at@gmail.com>
2021-04-19 17:35:29 -0500

    Add files via upload

---
+++ infoMarco/infoMarco/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  infoMarco
+//
+//  Created by Karina Amador Teran on 19/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfContrasena: UITextField!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
