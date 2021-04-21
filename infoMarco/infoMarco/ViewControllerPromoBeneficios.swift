6621dd6163f5a36eaee9e07a7353097c2b7733cd
Luis Mario <luis.mario.trujillo@live.com.mx>
2021-04-21 17:15:15 -0500

    Added new view controller

---
+++ infoMarco/infoMarco/ViewControllerPromoBeneficios.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerPromoBeneficios.swift
+//  infoMarco
+//
+//  Created by Luis Mario Trujillo on 21/04/21.
+//
+
+import UIKit
+
+class ViewControllerPromoBeneficios: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
