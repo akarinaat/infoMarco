26dd777bee0744686e8c14159cd2d8d335b286f2
AntonyTC <49083894+A01561769@users.noreply.github.com>
2021-05-10 18:02:30 -0700

    Avance de navegación
    Ahora se puede navegar entre las 3 principales pestanias mediante un Tab Bar Controller y ya hay verificación de formato de correo y de campos vacíos para el log in

---
+++ infoMarco/infoMarco/infoMarco/ViewControllerLectorEventos.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerLectorEventos.swift
+//  infoMarco
+//
+//  Created by Gerardo Ángeles Hernández on 22/04/21.
+//
+
+import UIKit
+
+class ViewControllerLectorEventos: UIViewController {
+    
+    @IBOutlet weak var tfQRCode: UITextField!
+    
+    var qrCodeString: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToLectorEventosScreen(segue: UIStoryboardSegue){
+        
+        tfQRCode.text = qrCodeString!
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
