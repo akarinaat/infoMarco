4f85ef55a76d5b8251e108cddf32d6d1b862e883
Gerardo Ángeles Hernández <gerardo788@live.com>
2021-05-23 00:17:13 -0500

    es para agregar los metodos faltantes

--- infoMarco/infoMarco/ViewControllerLectorEventos.swift
+++ infoMarco/infoMarco/ViewControllerLectorEventos.swift
@@ -14,6 +14,7 @@ class ViewControllerLectorEventos: UIViewController {
     
     var qrCodeString: String!
     var nombreEvento = ""
+    var cambioparamerge = ""
     
     
     override func viewDidLoad() {
