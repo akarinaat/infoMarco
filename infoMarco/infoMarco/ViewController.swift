bf706fa1eb41792879728e82a66ab51321342450
AntonyTC <A01561769@itesm.mx>
2021-05-25 13:19:37 -0700

    LogIn de admin y usuario sin switch

--- infoMarco/infoMarco/ViewController.swift
+++ infoMarco/infoMarco/ViewController.swift
@@ -7,16 +7,38 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfContrasena: UITextField!
-    @IBOutlet weak var switchBORRAR: UISwitch!
+    
+    var ref: DatabaseReference?
+    var arrAdmins = [Administrador]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Extraer usuarios administradores de la base de datos
+        ref = Database.database().reference().child("admnUsrs")
+        ref?.observe(DataEventType.value, with: {(snapshot) in
+            if snapshot.childrenCount>0{
+
+                for admins in snapshot.children.allObjects as! [DataSnapshot] {
+                    
+                    let adminObj = admins.value as? [String:String]
+                    let adminEmail = adminObj?["correo"]
+                    
+                    let adminMarco = Administrador(email: adminEmail ?? "")
+                
+                    self.arrAdmins.append(adminMarco)
+                    
+                }
+            }
+        }
+        )
     }
     
     // Funcion para validar que los tf tengan datos y que el tf de correo tenga el formato correcto
@@ -59,26 +81,43 @@ class ViewController: UIViewController {
         }
     }
     
+    func findAdmin (email: String) -> Bool {
+        
+        for admin in arrAdmins {
+            
+            if admin.email == email {
+                
+                return true
+                
+            }
+        }
+        
+        return false
+        
+    }
+    
     // MARK: - Navigation
     
     // Determinar si se logeará un usuario o un administrador y evitar ejecucion de segue validando si se escribe un correo valido y una contrasenia
     @IBAction func AdminOrUser(_ sender: Any) {
         
-        let correo = tfEmail.text!
-        let contras = tfContrasena.text!
-        
-        if !switchBORRAR.isOn{
+        let correo = tfEmail.text!                  // String con el correo del usuario (admin)
+        let contras = tfContrasena.text!            // Strong con la contra del usuario (admin)
+
+        // Verificar si el correo introducido es de un administrador
+        if !findAdmin(email: correo) {
             
-            //if identifier == "LogIn"{
+            // Validar formato de correo
             if validarCorreoContra(email: correo, contra: contras){
                     
-                print("Valido") // Imprimir si el correo es valido
                 
+                // Autenticar cuenta del usuario
                 Auth.auth().signIn(withEmail: correo, password: contras) {
                     (result, error) in
-                    
+                
                     if let result = result, error == nil {
                         
+                        // Ejecutar segue para el usuario
                         self.performSegue(withIdentifier: "LogIn", sender: self)
                         
                     } else {
@@ -93,27 +132,36 @@ class ViewController: UIViewController {
                     }
                 }
                 
-            } else {
-                
-                print("Invalido") // Imprimir si el correo NO es valido
-                
             }
             
         } else {
             
-            //if identifier == "LogIn"{
+            // Validar formato de correo
             if validarCorreoContra(email: tfEmail.text!, contra: tfContrasena.text!){
+                
             
-            self.performSegue(withIdentifier: "LogInAdmin", sender: self)
-            
-            } else {
-            
-                print("Invalido") // Imprimir si el correo NO es valido
-            
+                // Autenticar cuenta del usuario admin
+                Auth.auth().signIn(withEmail: correo, password: contras) {
+                    (result, error) in
+                    
+                    if let result = result, error == nil {
+                        
+                        // Ejecutar segue para el usuario admin
+                        self.performSegue(withIdentifier: "LogInAdmin", sender: self)
+
+                    } else {
+                        
+                        // Crear y desplegar alerta
+                        let alerta = UIAlertController(title: "ERROR", message: "Correo inexistente o contraseña incorrecta", preferredStyle: .alert)
+                        let accion = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        
+                        alerta.addAction(accion)
+                        self.present(alerta, animated: true, completion: nil)
+                        
+                    }
+                }
             }
-            
         }
-        
     }
     
     
